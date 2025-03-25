3152c3bc47ccfa207502bb7d03b456b7b383653a
Satyam <www.satyam200413@gmail.com>
2025-03-25 09:13:58 +0530

    Resolve webhook file with error handling

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -20,36 +20,50 @@ export const clerkWebhooks = async (req,res)=> {
         //Switch case for different events
         switch (type) {
             case 'user.created':{
-                const userData = {
-                    _id:data.id,
-                    email:data.email_addresses[0].email_address,
-                    name:data.first_name + " "+data.last_name,
-                    image:data.image_url,
-                    resume: ""
+                try {
+                    const userData = {
+                        _id:data.id,
+                        email:data.email_addresses[0].email_address,
+                        name:data.first_name + " "+data.last_name,
+                        image:data.image_url,
+                        resume: ""
+                    }
+                    await User.create(userData)
+                    console.log(User);
+                    
+                    res.json({})
+                    break;
+                } catch (error) {
+                    console.log(error.message);
+                    
                 }
-                await User.create(userData)
-                console.log(User);
-                
-                res.json({})
-                break;
             }
             case 'user.updated':{
 
-                const userData = {
-                    email:data.email_addresses[0].email_address,
-                    name:data.first_name + " "+data.last_name,
-                    image:data.image_url,
+                try {
+                    const userData = {
+                        email:data.email_addresses[0].email_address,
+                        name:data.first_name + " "+data.last_name,
+                        image:data.image_url,
+                        
+                    }
+                    await User.findByIdAndUpdate(data.id,userData)
+                    res.json({})
+                    break;
+                } catch (error) {
+                    console.log(error.message);
                     
                 }
-                await User.findByIdAndUpdate(data.id,userData)
-                res.json({})
-                break;
             }
             case 'user.deleted':{
 
-                await User.findByIdAndDelete(data.id)
+                try {
+                    await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
+                } catch (error) {
+                    console.log(error.message);
+                }
             }
                 
             default :
