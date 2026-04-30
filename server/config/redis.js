2ae73e4a65cc43a94f058abad69ed3a0215a22d0
Satyam <www.satyam200413@gmail.com>
2026-04-30 12:41:23 +0530

    configured the redis connection

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,26 +1,20 @@
-import {createClient} from 'redis';
+import { createClient } from "redis";
 
 const redisClient = createClient({
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-
-
-  },
-  password: process.env.REDIS_PASSWORD,
+  url: process.env.REDIS_URL
 });
 
-redisClient.on('error', (err) => {
-  console.error('Redis Client Error', err);
+redisClient.on("error", (err) => {
+  console.log("Redis Error:", err);
 });
 
 const connectRedis = async () => {
   try {
     await redisClient.connect();
-    console.log('Connected to Redis');
+    console.log("✅ Redis connected");
   } catch (error) {
-    console.error('Failed to connect to Redis:', error);
+    console.error("❌ Redis connection failed:", error);
   }
 };
 
-export {redisClient, connectRedis};
+export { redisClient, connectRedis };
