b0997d9fd4aa0ba50084dcf33a8262ee5aef3b5a
Satyam <www.satyam200413@gmail.com>
2026-05-02 13:17:45 +0530

    updated server.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
 
-app.get('/', (req, res) => res.send('API Working'))
+app.get('/', (req, res) => res.send('API Working Fine!'))
 
 app.get('/debug-sentry', function mainHandler(req, res) {
   throw new Error('My first Sentry error!')
